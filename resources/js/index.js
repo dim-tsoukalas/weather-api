f3b6effcdce2534efb2c8644f8496b5c9cad8104
dim-tsoukalas <dtsoukalas09@gmail.com>
2022-08-05 16:51:21 +0300

    Show only integers at temperature and added celcius symbol.

--- resources/js/index.js
+++ resources/js/index.js
@@ -7,7 +7,7 @@ var description = document.querySelector('#description')
 apiKey = '1ad0a30a3b2d659a6271b81f871d1861'
 
 function toCelcius(temper) {
-    return temper - 273;
+    return Math.trunc(temper - 273);
 }
 
 btn.addEventListener('click', function () {
@@ -31,7 +31,7 @@ btn.addEventListener('click', function () {
             var getDes = data['weather']['0']['description']
 
             document.getElementById("city").innerHTML = `Weather of <span>${getName}</span>`
-            document.getElementById("temp").innerHTML = `Temperature: <span>${toCelcius(getTemp)}</span>`
+            document.getElementById("temp").innerHTML = `Temperature: <span>${toCelcius(getTemp)}&#8451</span>`
             document.getElementById("description").innerHTML = `Description: <span>${getDes}</span>`
 
         })
