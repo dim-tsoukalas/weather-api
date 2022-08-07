210907a5e9c4306496b6b027f03f1850f0d84c10
dim-tsoukalas <dtsoukalas09@gmail.com>
2022-08-07 13:17:00 +0300

    Added commenting

--- resources/js/index.js
+++ resources/js/index.js
@@ -4,14 +4,26 @@ var city = document.querySelector('#city')
 var temp = document.querySelector('#temp')
 var description = document.querySelector('#description')
 
+// key from openweathermap
 apiKey = '1ad0a30a3b2d659a6271b81f871d1861'
 
+/*
+    --Kelvin to Celsius--
+    Function to convert inserted Kelvin temperature to
+    Celsius temperature.
+*/
 function toCelcius(temper) {
     return Math.trunc(temper - 273);
 }
 
-btn.addEventListener('click', function () {
+/*
+    --Event Listerer Search Button--
+    When user press search button this function is called.
+*/
 
+btn.addEventListener('click', function () {
+    
+    /*The url that takes data. -input.value->city, -apiKey->key */ 
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + input.value + '&appid=' + apiKey)
         .then(response => {
             //handle response            
@@ -24,12 +36,13 @@ btn.addEventListener('click', function () {
             }
         })
         .then(data => {
-            //handle data
 
+            //handle data
             var getName = data['name']
             var getTemp = data['main']['temp']
             var getDes = data['weather']['0']['description']
-
+            
+            //Send to Html
             document.getElementById("city").innerHTML = `Weather of <span>${getName}</span>`
             document.getElementById("temp").innerHTML = `Temperature: <span>${toCelcius(getTemp)}&#8451</span>`
             document.getElementById("description").innerHTML = `Description: <span>${getDes}</span>`
