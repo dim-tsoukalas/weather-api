7d868c012ae8bd28f86f40e5bf5901da2fd46bf6
dim-tsoukalas <dtsoukalas09@gmail.com>
2022-08-08 13:03:18 +0300

    When user adds a new city the order ones still there.

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,10 +1,15 @@
-var input = document.querySelector('#cityinput')
-var btn = document.querySelector('#search')
-var city = document.querySelector('#city')
-var temp = document.querySelector('#temp')
-var description = document.querySelector('#description')
+let input = document.querySelector('#cityinput')
+let btn = document.querySelector('#search')
+let city = document.querySelector('#city')
+let temp = document.querySelector('#temp')
+let description = document.querySelector('#description')
 
 var msg = document.getElementsByClassName('#msg')
+
+const container = document.getElementsByClassName('box')
+
+//focus
+input.focus()
 
 // key from openweathermap
 apiKey = '1ad0a30a3b2d659a6271b81f871d1861'
@@ -20,13 +25,14 @@ function toCelcius(temper) {
 
 /*  --Event Listerers Search Button--
     When user press search button or 'Enter' calls function searchAction().*/
-btn.addEventListener('click', function () {searchAction();})
-input.addEventListener('keypress', function(event){
-    if (event.key == 'Enter') {searchAction()}});
-
+btn.addEventListener('click', function () { searchAction(); })
+input.addEventListener('keypress', function (event) {
+    if (event.key == 'Enter') { searchAction() }
+});
+let i = 0
 //Search city
 function searchAction() {
-    /*The url that takes data. -input.value->city, -apiKey->key */ 
+    /*The url that takes data. -input.value->city, -apiKey->key */
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + input.value + '&appid=' + apiKey)
         .then(response => {
             //handle response            
@@ -40,27 +46,41 @@ function searchAction() {
             }
         })
         .then(data => {
-
             //handle data
             var getName = data['name']
             var getTemp = data['main']['temp']
             var getDes = data['weather']['0']['description']
             var iconcode = data['weather'][0]['icon']
             var getCountry = data['sys']['country']
-
-            //Send to Html
-            document.getElementById("city").innerHTML = getName
-            document.getElementById("temp").innerHTML = `${toCelcius(getTemp)}&#8451`
-            document.getElementById("description").innerHTML = getDes
             var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png"
-            show_icon(iconurl)
-            show_country(getCountry)
+            
+            //Send to Html
+            const card = document.createElement('div');
+            card.classList = 'templ';
+
+            const content = `<div class='ch' id=we-${i}>
+            <p id="city">${getName + ' ' + getCountry}</p>
+            <p id="temp">${toCelcius(getTemp)}&#8451</p>
+            <div id="icon"><img src='${iconurl}' id="wicon"></div>
+            <p id="description">${getDes.toUpperCase()}</p>
+            </div>
+            `;
+            console.log(iconurl)
+            i++;
+            
+            {$('.box').append(content);}
+            
+
         })
         .catch(error => {
             //handle error
-                showmess();
-          
+            showmess(error);
+            
         });
+
+    input.focus()
+    input.value = ""
+
 }
 
 
