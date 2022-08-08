2662268a17b56f2008e9fd852cdf161a266f6b1f
dim-tsoukalas <dtsoukalas09@gmail.com>
2022-08-08 10:33:43 +0300

    Icons, Country
    -Show weather icon
    -Show country of the city

--- resources/js/index.js
+++ resources/js/index.js
@@ -6,7 +6,6 @@ var description = document.querySelector('#description')
 
 var msg = document.getElementsByClassName('#msg')
 
-
 // key from openweathermap
 apiKey = '1ad0a30a3b2d659a6271b81f871d1861'
 
@@ -46,12 +45,16 @@ function searchAction() {
             var getName = data['name']
             var getTemp = data['main']['temp']
             var getDes = data['weather']['0']['description']
+            var iconcode = data['weather'][0]['icon']
+            var getCountry = data['sys']['country']
 
             //Send to Html
             document.getElementById("city").innerHTML = getName
             document.getElementById("temp").innerHTML = `${toCelcius(getTemp)}&#8451`
             document.getElementById("description").innerHTML = getDes
-
+            var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png"
+            show_icon(iconurl)
+            show_country(getCountry)
         })
         .catch(error => {
             //handle error
